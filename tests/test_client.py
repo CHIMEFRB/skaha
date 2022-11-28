7fdd11c3023dabffc48214d410aeadad19805c55
Oswaldo Alcalá <odarotto@gmail.com>
2022-11-28 12:58:21 -0400

    refactor(Client): Implementing pydantic on Client class and deprecating attrs on classes that inherit from Client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,15 +1,58 @@
 """Test Skaha Client API."""
 
 import pytest
+import requests
 
+from pydantic import BaseModel, ValidationError
 from skaha.client import SkahaClient
 from skaha.exceptions import InvalidCertificateError, InvalidServerURL
 
 
-def test_bad_server():
-    """Test bad server."""
+def test_client_has_session_attribute():
+    """Test if it SkahaClient object contains requests.Session attribute"""
+    client = SkahaClient()
+    assert hasattr(client, "session")
+    assert isinstance(client.session, requests.Session)
+
+
+def test_client_session():
+    """Test SkahaClient object's session attribute contains ther right headers"""
+    headers = [
+        "X-Skaha-Server",
+        "Content-Type",
+        "Accept",
+        "User-Agent",
+        "Date",
+        "X-Skaha-Version",
+        "X-Skaha-Client-Python-Version",
+        "X-Skaha-Client-Arch",
+        "X-Skaha-Client-OS",
+        "X-Skaha-Client-OS-Version",
+        "X-Skaha-Client-Platform",
+    ]
+    client = SkahaClient()
+    assert any(list(map(lambda h: h in client.session.headers.keys(), headers)))
+
+
+def test_bad_server_no_schema():
+    """Test server URL without schema."""
     with pytest.raises(InvalidServerURL):
-        SkahaClient(server="abcdefd")
+        SkahaClient(server="ws-uv.canfar.net")
+
+
+def test_bad_server_no_host():
+    """Test server URL without proper host."""
+    with pytest.raises(InvalidServerURL):
+        SkahaClient(server="https:///ea")
+
+
+def test_default_certificate():
+    """Test validation with default certificate value."""
+    try:
+        SkahaClient()
+    except ValidationError:
+        assert False
+    assert True
 
 
 def test_bad_certificate():
@@ -22,8 +65,3 @@ def test_bad_certificate_path():
     """Test bad certificate."""
     with pytest.raises(InvalidCertificateError):
         SkahaClient(certificate="/tmp/abcdefd")
-
-
-def test_client_headers():
-    """Test client headers."""
-    assert SkahaClient().headers["Content-Type"] == "application/json"
